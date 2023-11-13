033296c261980c5d37858126ee616b5e12d1dac2
bettayub <bettayub19@gmail.com>
2023-11-13 22:11:06 +0300

    Adding data to portfolio

---
+++ src/data/portfolio.js
@@ -0,0 +1,22 @@
+export default [
+   {
+       title: 'Personal Website',
+       imgUrl: 'assets/portfolio.jpg',
+       stack: ['HTML', 'CSS', 'JavaScript'],
+       link: 'https://bettayub.github.io/my-personal-website/',
+   },
+
+   {
+       title: 'Lost and found frontend',
+       imgUrl: 'assets/lost1.jpg',
+       stack: ['Vite', 'Tailwind', 'React'],
+       link: 'https://team-lost-and-found-frontend-4486.vercel.app/',
+   },
+
+    {
+       title: 'Lost and found backend',
+       imgUrl: 'assets/api.jpg',
+       stack: ['Python', ' Flask-SQLAlchemy', 'Flask-RESTx'],
+       link: 'https://lostandfoundbackend-o0al.onrender.com/',
+   },
+]
